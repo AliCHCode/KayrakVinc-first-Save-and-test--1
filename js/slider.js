c8516069987fdded444a5313f31edb1e792d48b1
Bedirhan KARA <bedirhank.bk41@gmail.com>
2026-07-10 23:07:44 +0300

    slider otomatik geçiş yapıldı

--- js/slider.js
+++ js/slider.js
@@ -7,9 +7,31 @@
     const prevBtn = slider.querySelector(".slider-arrow-left");
     const nextBtn = slider.querySelector(".slider-arrow-right");
     const dots = slider.querySelectorAll(".slider-dot");
+    const video = slider.querySelector(".slide video");
 
     let currentIndex = 0;
     const slideCount = slides.length;
+
+    const IMAGE_DURATION = 4000;
+    const VIDEO_DURATION = 22000;
+
+    let timer = null;
+
+    function scheduleAutoAdvance() {
+        clearTimeout(timer);
+
+        if (currentIndex === 1) {
+            // Video slide: restart video, switch back to image after 22s
+            if (video) {
+                video.currentTime = 0;
+                video.play().catch(() => {});
+            }
+            timer = setTimeout(() => goToSlide(0), VIDEO_DURATION);
+        } else {
+            // Image slide: switch to video after 4s
+            timer = setTimeout(() => goToSlide(1), IMAGE_DURATION);
+        }
+    }
 
     function goToSlide(index) {
         currentIndex = (index + slideCount) % slideCount;
@@ -17,6 +39,7 @@
         dots.forEach((dot, i) => {
             dot.classList.toggle("active", i === currentIndex);
         });
+        scheduleAutoAdvance();
     }
 
     prevBtn.addEventListener("click", () => goToSlide(currentIndex - 1));
@@ -27,4 +50,8 @@
             goToSlide(Number(dot.dataset.index));
         });
     });
+
+    // Start the automatic cycle on the initial image slide
+    scheduleAutoAdvance();
 })();
+
